11856003a75012dd68619413fb83f4dfa0eb1634
Git-Sevkv <75123735+Git-Sevkv@users.noreply.github.com>
2020-11-27 20:39:02 +0300

    Add files via upload

---
+++ individ_edit.php
@@ -0,0 +1,46 @@
+﻿
+
+<?php
+
+//print_r($_GET);
+
+$Link=mysql_connect('u464554.mysql.masterhost.ru','u464554','c_m4sSIOTi');
+
+if(!$Link)die('Нет подключения к БД!');
+
+@mysql_query('SET NAMES utf8');
+
+mysql_select_db('u464554');
+
+if($_GET["btn_e"]=="Зарегистрироваться")
+   {$_GET["id"]=1;
+	$q=mysql_query("select max(id)+1 as mxid from CAV_individ");
+	while ($str=mysql_fetch_array($q,MYSQL_ASSOC)){if($str["mxid"]!="")$_GET["id"]=$str["mxid"];}
+
+echo "insert into CAV_individ (id,fam,name,s_name,date_of_brth,login,password) 
+		   values(".$_GET["id"].",
+		   ".$_GET["fam"].",
+		   ".$_GET["name"].",
+		   ".$_GET["s_name"].",
+		   ".$_GET["date_of_brth"].",
+		   ".$_GET["login"].",
+		          '".$_GET["password"]."')";
+    $q=mysql_query("insert into CAV_individ (id,fam,name,s_name,date_of_brth,login,password) 
+		   values(".$_GET["id"].",
+		   '".$_GET["fam"]."',
+		   '".$_GET["name"]."',
+		   '".$_GET["s_name"]."',
+		   '".$_GET["date_of_brth"]."',
+		   '".$_GET["login"]."',
+		          '".$_GET["password"]."')");
+
+
+     echo "<h3>Добавлен: ".$_GET["fam"]."</h3>";
+    }
+	
+	
+	
+
+?>
+
+ <p><a href="registration.php">Вернуться назад</a></p> 
---
+++ sign_up.php
@@ -0,0 +1,31 @@
+﻿<h1 style="position: absolute;left: 1%;top:1%;">Выберите врача</h1>
+<?php
+$Link=mysql_connect('u464554.mysql.masterhost.ru','u464554','c_m4sSIOTi');
+
+if(!$Link)die('Нет подключения к БД!');
+
+@mysql_query('SET NAMES utf8');
+
+mysql_select_db('u464554');
+
+$q=mysql_query("SELECT CAV_doctor.* ,
+		CAV_individ.fam
+		from CAV_doctor 
+		LEFT JOIN CAV_individ ON CAV_individ.id=CAV_doctor.id_ind_doc");
+while ($qq[]=mysql_fetch_array($q,MYSQL_ASSOC)){}
+
+echo "<select onclick=\"document.getElementById('fr_sp').src='calendar/index.php?id_ind='+this.value;\">";
+foreach($qq as $r)
+	if($r["fam"]!="")
+		{
+		echo "<option value=".$r["id"].">";
+		echo $r["doctor"]." ".$r["fam"];
+		echo "</option>";
+		}
+echo "</select>";
+
+	  
+?>
+<iframe id=fr_sp src="index.php?id=1"
+        style="position: absolute;left: 1%;top: 10%;width:98%;height:80%;"
+></iframe>
