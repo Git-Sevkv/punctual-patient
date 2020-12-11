16ed5e71f308601249728ebd546d739a8abef6b4
Git-Sevkv <75123735+Git-Sevkv@users.noreply.github.com>
2020-12-12 01:37:43 +0300

    Add files via upload

---
+++ med_look.php
@@ -0,0 +1,51 @@
+﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿  <?php
+$Link=mysql_connect('u464554.mysql.masterhost.ru','u464554','c_m4sSIOTi');
+
+if(!$Link)die('Нет подключения к БД!');
+
+@mysql_query('SET NAMES utf8');
+
+mysql_select_db('u464554');
+$q=mysql_query("SELECT * from CAV_doctor where exists(SELECT * from CAV_pac where CAV_pac.id_doc=CAV_doctor.id)");
+unset($qq1);				
+while ($qq1[]=mysql_fetch_array($q,MYSQL_ASSOC)){}
+
+$q=mysql_query("SELECT * from CAV_pac");
+unset($qqp);				
+while ($qqp[]=mysql_fetch_array($q,MYSQL_ASSOC)){}
+
+
+echo"<table>
+<tr>
+  <th align=center style=\"width:200;height:40;border-style:solid;border-width:1;background-color: #77dd77\";></th>";
+
+foreach ($qq1 as $r1)
+	if($r1["id"]!="")
+		echo"<th align=center style=\"width:200;height:40;border-style:solid;border-width:1;background-color: #77dd77\";><h2>".$r1["doctor"]."</h2></th>";
+   
+ echo"</tr>";
+  
+$q=mysql_query("SELECT * from CAV_individ where exists(SELECT * from CAV_pac where CAV_pac.id_ind_pac=CAV_individ.id)");
+unset($qq);				
+while ($qq[]=mysql_fetch_array($q,MYSQL_ASSOC)){}
+
+foreach ($qq as $r)
+	if($r["id"]!="") 
+		{
+		 echo"<tr><td align=center style=\"width:200;height:40;border-style:solid;border-width:1;background-color: #f5e8d0;\">".$r["fam"]."</td>";
+		 foreach ($qq1 as $r1)
+			if($r1["id"]!="")
+			{$find_pac=0;
+			 foreach ($qqp as $rp)
+				if(($r1["id"]==$rp["id_doc"])&&($r["id"]==$rp["id_ind_pac"]))
+					{$find_pac=1;
+					echo"<td align=center style=\"width:200;height:40;border-style:solid;border-width:1;background-color: #f5e8d0;\">".$rp["time_way"]."</td>";
+					}	
+			if($find_pac==0)echo"<td align=center style=\"width:200;height:40;border-style:solid;border-width:1;background-color: #f5e8d0;\"></td>";
+			}
+		 echo"</tr>";
+		}
+echo"
+</table>
+";  
+  ?>
---
+++ par_and_xar.php
@@ -0,0 +1,70 @@
+﻿  <?php
+$Link=mysql_connect('u464554.mysql.masterhost.ru','u464554','c_m4sSIOTi');
+
+if(!$Link)die('Нет подключения к БД!');
+
+@mysql_query('SET NAMES utf8');
+
+mysql_select_db('u464554');
+
+$q=mysql_query("SELECT CAV_xar.*,CAV_param.name from CAV_xar
+				left join CAV_param on CAV_param.id=CAV_xar.id_par
+				where id_parent is NULL");
+unset($qqp);				
+while ($qqp[]=mysql_fetch_array($q,MYSQL_ASSOC)){}
+
+
+echo"<table>";
+ $i_n=1; 
+ foreach ($qqp as $rp)
+	if($rp["id"]!="")
+	{    
+	 echo"<tr><td>".$i_n."</td>";
+	 echo"<td align=center style=\"width:200;height:40;border-style:solid;border-width:1;background-color: #f5e8d0;\">".$rp["name"]."</td>";
+	 echo"<td align=center style=\"width:200;height:40;border-style:solid;border-width:1;background-color: #f5e8d0;\">".$rp["val"]."</td>";
+	 echo"</tr>";
+	 
+	 $q=mysql_query("SELECT CAV_xar.*,CAV_param.name from CAV_xar
+				left join CAV_param on CAV_param.id=CAV_xar.id_par
+				where id_parent=".$rp["id"]);
+unset($qqp1);				
+while ($qqp1[]=mysql_fetch_array($q,MYSQL_ASSOC)){}
+
+echo"<tr><td></td><td colspan=2>";
+echo"<table>";
+  
+ foreach ($qqp1 as $rp1)
+	if($rp1["id"]!="")
+	{    
+	 echo"<tr>";
+	 echo"<td align=center style=\"width:200;height:40;border-style:solid;border-width:1;background-color: #f5e8d0;\">".$rp1["name"]."</td>";
+	 echo"<td align=center style=\"width:200;height:40;border-style:solid;border-width:1;background-color: #f5e8d0;\">".$rp1["val"]."</td>";
+	 echo"</tr>";
+	}
+	
+echo"</table>";
+echo"</td></tr>";
+$i_n++;
+	}
+	
+$q=mysql_query("select * from CAV_param
+
+				");
+unset($qqp);				
+while ($qqp[]=mysql_fetch_array($q,MYSQL_ASSOC)){}
+
+echo "<tr><td colspan=4><form action=\"par_and_xar.php\"> 
+<p>Параметр<select id=id_par name=id_par>";
+
+foreach($qqp as $rp)
+	if($rp["name"]!="")
+		{
+echo "<option value=".$rp["id"].">";
+		echo $rp["name"];
+		echo "</option>";
+		}
+echo "</select></p>
+<p>Значение<input id=val name=val></p>
+<input id=btn_xar name=btn_xar type=submit value=\"Добавить строку\"></form></td></tr>";
+echo"</table>";
+  ?>
